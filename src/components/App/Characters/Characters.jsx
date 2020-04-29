0cfdc9a714a0659e66aee658fbc205d626bb985f
surelis segarra <sdarianne34@gmail.com>
2020-04-29 15:46:35 -0700

    code for character is in the correct file

--- src/components/App/Characters/Characters.jsx
+++ src/components/App/Characters/Characters.jsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 
-const Character =({ name, image }) => (
-  <figure>
-    <img src={image} alt=''/>
-    <figcaption>
-      <h1>{name}</h1>
-    </figcaption>
-  </figure>
-);
-
-Character.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired
-};
-
-export default Character;
